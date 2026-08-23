0d47af6e6809c1ade223268af06be122c3f80b85
Ankit-jakhar-4885 <ankitjakhar4885@gmail.com>
2026-08-23 22:06:52 +0530

    Update Brokerage.js

--- Brokerage.js
+++ Brokerage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 function Brokerage() {
-    return ( 
+    return (
       <div className='container'>
         <div className='row p-5  mt-5  text-center   border-top '  >
               <div className='col-8 p-4'> 
